0c8062225a3168b287cac54fa7ca50afe8e4bde8
Ivan Stevkovski <istevkovski@outlook.com>
2023-08-27 22:15:52 +0200

    chore: prettier yml override, examples deploy

--- src/examples/vite.config.ts
+++ src/examples/vite.config.ts
@@ -2,22 +2,23 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-	open: "/src/examples/index.html"
-  },
-  build: {
-	rollupOptions: {
-		input: {
-			main: "/src/examples/index.tsx"
-		}
-	}
-  },
-  resolve: {
-    alias: {
-      "components": "/src/components",
-      "hooks": "/src/hooks",
-      "types": "/src/types",
-    }
-  },
+	plugins: [react()],
+	server: {
+		open: '/src/examples/index.html',
+	},
+	build: {
+		outDir: 'src/examples/build',
+		rollupOptions: {
+			input: {
+				main: '/src/examples/index.tsx',
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			components: '/src/components',
+			hooks: '/src/hooks',
+			types: '/src/types',
+		},
+	},
 });
